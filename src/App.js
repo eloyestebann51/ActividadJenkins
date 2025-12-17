f0dcb705655f2bc5bc05d69bf15ab7a1b8038a1b
Eloy Esteban <eloymax51@gmail.com>
2025-12-17 13:28:53 +0100

    Primer Cambio

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Input } from 'reactstrap';
 import React from 'react';
 
+// Comentario de prueba
 class App extends React.Component {
   constructor() {
     super();
