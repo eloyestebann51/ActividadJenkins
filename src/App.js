050aa125a149c3cc4699867bd8d6fe3c8ccddab2
Eloy Esteban <eloymax51@gmail.com>
2025-12-17 13:33:56 +0100

    Cambio Automatico 2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Input } from 'reactstrap';
 import React from 'react';
 
-// Comentario de prueba 2
+// Comentario de prueba 3
 class App extends React.Component {
   constructor() {
     super();
